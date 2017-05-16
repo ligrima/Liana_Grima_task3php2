571e77e75047ecb2af4fbabdc72594a0b7321d2d
ligrima <lianagrima@gmail.com>
2017-05-16 16:37:03 +0200

    added sass and js
    added sass and js files, partials - ok sass, js not yet

---
+++ Liana_Grima_task3php2_ci2/_data/js/webapp.js
@@ -0,0 +1,12 @@
+//@koala-prepend 'partials/notes.js'
+//@koala-prepend 'partials/calendar.js'
+//@koala-prepend 'partials/nav4app.js'
+//@koala-prepend 'partials/timetable.js'
+
+// This activates after the page is done loading
+// NOTE: YOU ONLY NEED THIS ONCE.
+$(function () {
+
+    calendar.build (2017, 2);
+
+});
