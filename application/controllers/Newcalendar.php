ad22bc45cb5dd02c51a86b5e705a1822a2b7fa1a
ligrima <lianagrima@gmail.com>
2017-05-28 16:56:57 +0200

    calendar
    the new calendar page is fully

--- application/controllers/Newcalendar.php
+++ application/controllers/Newcalendar.php
@@ -6,7 +6,20 @@ class Newcalendar extends CI_Controller {
 
     function display($year = null, $month = null) {
 
+        if (!$year) {
+            $year = date ('Y');
+        }
+        if (!$month) {
+            $month = date ('m');
+        }
         $this->load->model('Newcalendar_Model');
+
+        if ($day = $this->input->post('day')) {
+            $this->Newcalendar_Model->add_calendar_data(
+                "$year-$month-$day",
+                $this->input->post('data')
+            );
+        }
         $data['calendar'] = $this->Newcalendar_Model->generate($year, $month);
 
         $this->load->view('newcalendar', $data);
